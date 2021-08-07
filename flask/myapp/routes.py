94f0d34b6ecb5be816c17ce4018055d1ba5117a6
shaunchuang <72535297+shaunchuang@users.noreply.github.com>
2021-08-08 02:36:07 +0800

    update

--- flask/myapp/routes.py
+++ flask/myapp/routes.py
@@ -1,8 +1,10 @@
 from myapp import app
 from myapp.forms import UseModelForm
-from flask import render_template
+from flask import render_template, request
 from myapp.model import predict, data_Collect, resultcompute
+from myapp.mysql_model import create_url
 from myapp.init_conn import connection
+import math
 
 
 @app.route('/')
@@ -32,10 +34,27 @@ def loan():
 
 @app.route('/model2result')
 def model2result():
-    with connection.cursor() as cursor:
-        sqltest = 'SELECT * FROM car_list LIMIT 9'
-        cursor.execute(sqltest)
-        result = cursor.fetchall()
-        car_url = None
 
-    return render_template('model2_result.html', result=result, car_url=car_url)
+    cr = connection.cursor()
+    sqlrows = 'SELECT COUNT(*) AS items FROM car_list where segment=1'
+    cr.execute(sqlrows)
+    rows = cr.fetchone()
+    page = request.args.get('page', 1, type=int)
+    items = rows['items']
+    totalpages = math.ceil(items/9)
+    limit = 9
+
+    if page < totalpages:
+        offset = page*limit - limit
+    if page > totalpages:
+        offset = totalpages*limit - limit
+
+    cur = connection.cursor()
+    sqltest = f'SELECT * FROM car_list where segment=1 LIMIT {limit} OFFSET {offset}'
+    cur.execute(sqltest)
+    result = cur.fetchall()
+
+    for r in result:
+        r['car_url'] = create_url(r)
+
+    return render_template('model2_result.html', result=result, page=page, totalpages=totalpages)
